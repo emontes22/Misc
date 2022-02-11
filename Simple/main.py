6342452ee5e210b7668d26c6275b7d44bf166337
Ervey Montes <ervey025@gmail.com>
2022-02-11 14:33:34 -0700

    Adding player character

--- Simple/main.py
+++ Simple/main.py
@@ -11,23 +11,28 @@ font = pygame.font.Font('Simple/font/DisposableDroidBB.ttf', 50)
 sky = pygame.image.load('Simple/images/Sky.jpg').convert_alpha()
 ground = pygame.image.load('Simple/images/Ground.jpg').convert_alpha()
 text = font.render('My Game', False, 'Black')
+
 enemy = pygame.image.load('Simple/images/enemy/snail1.png').convert_alpha()
-enemy_xpos = 600
+enemy_rect = enemy.get_rect(bottomright = (600, 300))
+
+player = pygame.image.load('Simple/images/player/player_walk_1.png').convert_alpha()
+player_rect = player.get_rect(midbottom = (80, 300))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        
     
     screen.blit(sky, (0, 0))
     screen.blit(ground, (0, 300))
     screen.blit(text, (300, 50))
-    enemy_xpos -= 3
-    if enemy_xpos <= -100:
-        enemy_xpos = 800
-    screen.blit(enemy, (enemy_xpos, 265))
+
+    enemy_rect.x -= 3
+    if enemy_rect.right <= 0:
+        enemy_rect.left = 800
+    screen.blit(enemy, enemy_rect)
+    screen.blit(player, player_rect)
 
     pygame.display.update()
     clock.tick(60)
