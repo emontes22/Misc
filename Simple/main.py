d2c89188487e4170871bffa7df5302ee6106e7b0
Ervey Montes <ervey025@gmail.com>
2022-02-11 13:51:49 -0700

    Added background images

--- Simple/main.py
+++ Simple/main.py
@@ -1,3 +1,4 @@
+from tkinter.filedialog import test
 import pygame
 from sys import exit
 
@@ -5,11 +6,22 @@ pygame.init()   #Starts pygame
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('First Game')    #Title of game
 clock = pygame.time.Clock()
+font = pygame.font.Font('Simple/font/DisposableDroidBB.ttf', 50)
+
+sky = pygame.image.load('Simple/images/Sky.jpg')
+ground = pygame.image.load('Simple/images/Ground.jpg')
+text = font.render('My Game', False, 'Black')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        
+    
+    screen.blit(sky, (0, 0))
+    screen.blit(ground, (0, 300))
+    screen.blit(text, (300, 50))
+
     pygame.display.update()
     clock.tick(60)
